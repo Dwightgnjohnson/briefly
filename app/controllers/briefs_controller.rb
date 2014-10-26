ac38e872d4f838845377c09ada59236d0c0bf379
dwightgnjohnson <dwightgnjohnson@gmail.com>
2014-10-25 19:01:03 -0500

    caught up to where jwo was for his DeepThoughts app

--- app/controllers/briefs_controller.rb
+++ app/controllers/briefs_controller.rb
@@ -1,5 +1,28 @@
 class BriefsController < ApplicationController
+
+  before_action do
+    if session[:user_id].present?
+    else
+      redirect_to sign_in_path
+    end
+  end
+
+
+
   def index
+    @users = User.all
+    @briefs = current_user.briefs.order("created_at desc")
+    @brief = Brief.new
+  end
+
+  def post
+    @post = Brief.new(params.require(:brief).permit(:brief))
+    @post.user = current_user
+    if @post.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
 
   root 'briefs#index'
+
+  post 'briefs' => 'briefs#post', as: "post_brief"
+
 
   get "sessions/new" => "sessions#new", as: "sign_in"
   post "sessions" => "sessions#create"
   delete "sessions" => "sessions#signout", as: "sign_out"
   get "signup" => "sessions#signup", as: :signup
   post "signup" => "sessions#create_user", as: :create_user
+
+get "users/:username" => "profiles#show", as: "profile"
+
 end
